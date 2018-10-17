bd8b34214f6ad55dc83aa97491fdeba9b63e4549
spuermax <13783141492@163.com>
2018-10-17 13:31:46 +0800

    Toast

---
+++ base/src/main/java/com/supermax/base/common/widget/toast/QsToast.java
@@ -0,0 +1,58 @@
+package com.supermax.base.common.widget.toast;
+
+import android.content.Context;
+import android.os.Looper;
+import android.support.annotation.StringRes;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.supermax.base.common.utils.QsHelper;
+
+/*
+ * @Author yinzh
+ * @Date   2018/10/17 13:30
+ * @Description
+ */
+public class QsToast {
+
+    private static Toast mToast = null;
+
+    /**
+     * 简单Toast 消息弹出
+     */
+    public static void show(final String msg) {
+        if (TextUtils.isEmpty(msg)) {
+            return;
+        }
+        // 判断是否在主线程
+        if (Looper.getMainLooper().getThread() != Thread.currentThread()) {
+            QsHelper.getInstance().getThreadHelper().getMainThread().execute(new Runnable() {
+                @Override public void run() {
+                    showToast(QsHelper.getInstance().getApplication(), msg, Toast.LENGTH_SHORT);
+                }
+            });
+        } else {
+            showToast(QsHelper.getInstance().getApplication(), msg, Toast.LENGTH_SHORT);
+        }
+    }
+
+    public static void show(@StringRes int resId) {
+        if (resId != 0) {
+            show(QsHelper.getInstance().getString(resId));
+        }
+    }
+
+    /**
+     * 弹出提示
+     */
+    private static void showToast(Context context, String text, int duration) {
+        if (mToast == null) {
+            mToast = Toast.makeText(context, text, duration);
+            mToast.show();
+        } else {
+            mToast.setText(text);
+            mToast.setDuration(duration);
+            mToast.show();
+        }
+    }
+}
