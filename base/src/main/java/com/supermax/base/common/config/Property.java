35b7e94bbede5792ea41eb8581829e2a313a0f2d
spuermax <13783141492@163.com>
2018-10-20 18:13:49 +0800

    Config

---
+++ base/src/main/java/com/supermax/base/common/config/Property.java
@@ -0,0 +1,17 @@
+package com.supermax.base.common.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/*
+ * @Author yinzh
+ * @Date   2018/10/20 17:47
+ * @Description
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Property {
+    String value() default "";
+}
---
+++ base/src/main/java/com/supermax/base/common/config/PropertyCallback.java
@@ -0,0 +1,10 @@
+package com.supermax.base.common.config;
+
+/*
+ * @Author yinzh
+ * @Date   2018/10/20 17:48
+ * @Description
+ */
+public interface PropertyCallback {
+    void onSuccess();
+}
