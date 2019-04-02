4a836b58744018fb582886004bfb42d5c65d55eb
spuermax <13783141492@163.com>
2019-04-02 19:55:25 +0800

    QsDialogFragment优化。

--- base/src/main/java/com/supermax/base/common/widget/dialog/QsDialogFragment.java
+++ base/src/main/java/com/supermax/base/common/widget/dialog/QsDialogFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 
+import com.supermax.base.common.log.L;
 import com.supermax.base.common.utils.QsHelper;
 import com.supermax.base.common.viewbind.ViewBindHelper;
 
@@ -78,14 +81,34 @@ public abstract class QsDialogFragment extends DialogFragment {
     protected abstract void initData();
 
     public void show() {
-        show(null);
+        show(QsHelper.getInstance().getScreenHelper().currentActivity(), null);
     }
 
     public void show(Bundle bundle) {
-        if (bundle != null) {
-            setArguments(bundle);
+        show(QsHelper.getInstance().getScreenHelper().currentActivity(), bundle);
+    }
+
+    public void show(FragmentActivity activity) {
+        show(activity, null);
+    }
+
+    public void show(FragmentActivity activity, Bundle bundle) {
+        if (activity == null || activity.isFinishing()) {
+            L.e(initTag(), "activity is null or activity is finished!");
+            return;
         }
-        QsHelper.getInstance().commitDialogFragment(this);
+        L.i(initTag(), "show......activity:" + activity.getClass().getSimpleName());
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        if (fragmentManager != null) {
+            if (isAdded()) {
+                L.e(initTag(), "show......dialog is added");
+                return;
+            }
+            if (bundle != null) setArguments(bundle);
+            fragmentManager.beginTransaction().add(this, getClass().getSimpleName()).commitAllowingStateLoss();
+        } else {
+            L.e(initTag(), "show......fragmentManager is null");
+        }
     }
 
 }
